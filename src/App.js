d4fe8e4e9bd923b83485fa5458c191106b7d0625
chriscortes4u <chriscortes4u@yahoo.com>
2016-10-18 11:49:53 -0400

    made components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 const h = require('react-hyperscript')
+var items = [{ id: 1,
+   title: 'JavaScript Fatigue',
+   link: 'https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.qtciu77bd',
+   score: 0
+ }]
 
-module.exports = () => h('div', 'Hello Mars')
+const Header = require('./components/Header.js')
+const List = require('./components/List.js')
+const Form = require('./components/Form.js')
+const props = {items: items}
+module.exports = () => h('div.pa4.bg-blue.vh-100',[
+ Header(),
+  h('main', [
+    List(props),
+    Form()
+ ])
+ ])
